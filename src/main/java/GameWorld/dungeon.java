4f2a80ba2387c9f97a024dda157e1b24cf83a7af
MartinaScorrano <martina.scorrano@student.univaq.it>
2025-12-11 10:38:01 +0100

    creazione classe Dungeon

--- src/main/java/GameWorld/dungeon.java
+++ src/main/java/GameWorld/dungeon.java
@@ -1,6 +1,15 @@
 package GameWorld;
 
 public class Dungeon {
+    private int width,height;
+    private Room[][]dungeon;
 
+public Dungeon (int width,int height){
+    this.width=width;
+    this.height=height;
+    dungeon=new Room [width][height];
+}
+public void generateDungeon(){
     
+}   
 }
--- src/main/prova2.java
+++
@@ -1,5 +0,0 @@
-public class prova2 {
-    public static void main ( String [] args ) {
-        System.out.println("Juri Di Rocco è il numero uno");
-    }
-}
