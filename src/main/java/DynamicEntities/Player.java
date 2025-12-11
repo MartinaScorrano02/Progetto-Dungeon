d9ed2d52ce5d430f8b9382deee259bd5fa4efa1d
fazi-michela <sansgirl654.skl@gmail.com>
2025-12-11 19:09:54 +0100

    aggiunto metodi base per Player

--- src/main/java/DynamicEntities/Player.java
+++ src/main/java/DynamicEntities/Player.java
@@ -1,3 +1,26 @@
-public class Player {
-    
+public class Player extends Entity {
+    private int lvl;
+    private int exp;
+    private int expToNextLvl;
+    private final Inventory inventory;
+
+    public Player(int maxHP,int attack, int defense, Position startPosition) {
+        super(maxHp, attack, defense, startPosition);
+        this.inventory = new Inventory();
+    }
+
+    public int getLevel() {
+        return this.lvl;
+    }
+
+    public int getExp() {
+        return this.exp;
+    }
+    public int getXP2NextLevel() {
+        return this.expToNextLvL;
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
 }
