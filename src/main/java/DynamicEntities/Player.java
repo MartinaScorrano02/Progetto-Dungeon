3eac9dff7befe95d9045ce071601fe4994b705d1
fazi-michela <sansgirl654.skl@gmail.com>
2025-12-11 19:26:20 +0100

    aggiunto metodi base per Player

--- src/main/java/DynamicEntities/Player.java
+++ src/main/java/DynamicEntities/Player.java
@@ -1,3 +1,5 @@
+import javax.swing.text.Position;
+
 public class Player extends Entity {
     private int lvl;
     private int exp;
@@ -23,4 +25,42 @@ public class Player extends Entity {
     public Inventory getInventory() {
         return this.inventory;
     }
+
+    public void setInventory(Inventory newInventory) {
+        this.inventory = newInventory;
+    }
+
+    public void move() {
+
+    }
+
+    public void gainExperience(int amount) {
+        this.experience += amount;
+        checkLevelUp();
+    }
+
+    public void levelUp() {
+        level++;
+        experience -= expToNextLvl;
+        expToNextLvl = calculateExperienceToNextLevel(level);
+
+        setMaxHP(getMaxHP() + calculateHPIncrease());
+        setAttack(getAttack() + calculateAttackIncrease());
+        setDefense(getDefense() + calculateDefenseIncrease());
+        setCurrentHP(getMaxHP());
+        onLevelUp();
+    }
+
+    public void useSpecialAbility(Character target) {
+    }
+
+    public boolean effectExpired() {
+        return true;
+    }
+
+    public int gainLevel() {
+        levelUp();
+        return level;
+    }
+    
 }
