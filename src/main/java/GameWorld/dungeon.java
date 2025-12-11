76a40a7f2b4ba05c56b82c17867f0ef65da35b54
For-Cycle <mattiapalladinelli@gmail.com>
2025-12-11 10:45:39 +0100

    creazione packages

--- src/main/java/GameWorld/dungeon.java
+++ src/main/java/GameWorld/dungeon.java
@@ -1,8 +1,12 @@
 package GameWorld;
 
+
 public class Dungeon {
     private int width,height;
     private Room[][]dungeon;
+
+public class dungeon {
+
 
 public Dungeon (int width,int height){
     this.width=width;
--- src/main/java/TrapsANDEvents/roomEvent.java
+++ src/main/java/TrapsANDEvents/roomEvent.java
@@ -1,4 +1,4 @@
-package Traps&Events;
+package traps&events;
 
 public class roomEvent {
     
