c1eb89b71fd18833c92c39404d285e707e5dccb8
fazi-michela <sansgirl654.skl@gmail.com>
2025-12-11 18:26:16 +0100

    aggiunto le variabili di Entity

--- src/main/java/DynamicEntities/entity.java
+++ src/main/java/DynamicEntities/entity.java
@@ -1,5 +1,9 @@
 package DynamicEntities;
 
-public class entity {
-    
+public class Entity implements Combatant, Interactable {
+    private int hp;
+    private int maxHP;
+    private int attack;
+    private int defense;
+    private Position position;
 }
