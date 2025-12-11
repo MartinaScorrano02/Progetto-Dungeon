c13893ad073d54f3cf903afe9ef82c76584e7b53
MartinaScorrano <martina.scorrano@student.univaq.it>
2025-12-11 18:50:10 +0100

    metodi classe Room

--- src/main/java/GameWorld/Room.java
+++ src/main/java/GameWorld/Room.java
@@ -22,9 +22,28 @@ public Room(int row,int col,String description,Passage north,Passage south,Passa
     this.isBossRoom=false;
     this.secretPassage=false;
     this.secretRoom=false;
-    characters=new LinkedList<>();
-    items=new LinkedList<>();
-    events=new LinkedList<>();
+    characters=new ArrayList<>();
+    items=new ArrayList<>();
+    events=new ArrayList<>();
+}
+public boolean addEnemy(Enemy enemy){
+for(Character character:characters){
+    if(character.getName().equals(enemy.getName())){
+        return false;
+    }
+    characters.add(enemy);
+    return true;
+}
 }
 
+public boolean addKey(Key key){
+    for(Item item:items){
+        if(item.getName().equals(key.getName())){
+            return false;
+        }
+        items.add(key);
+    }
 }
+
+
+
