75839d50e7e10093315be2be1aa2e940d4169995
fazi-michela <sansgirl654.skl@gmail.com>
2025-12-11 18:36:11 +0100

    aggiunto metodi getter

--- src/main/java/DynamicEntities/entity.java
+++ src/main/java/DynamicEntities/entity.java
@@ -6,4 +6,39 @@ public class Entity implements Combatant, Interactable {
     private int attack;
     private int defense;
     private Position position;
+
+    public Entity(int baseMaxHP, int baseAttack, int baseDefense, Position startPosition) {
+        this.maxHp = baseMaxHp;
+        this.hp = maxHP;
+        this.attack = baseAttack;
+        this.defense = baseDefense;
+        this.position = startPosition;
+    }
+
+     public int geHP() {
+        return this.hp;
+    }
+
+    public int getMaxHP() {
+        return this.maxHP;
+    }
+
+    public Position getPosition() {
+    return this.position;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+
+    protected void setMaxHP(int newMaxHP) {
+        this.maxHp = newMaxHp;
+    }
+
+
 }
