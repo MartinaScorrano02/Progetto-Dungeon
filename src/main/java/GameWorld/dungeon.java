98a4d9f74053493f814412da0d7b4bf1c27dcfff
MartinaScorrano <martina.scorrano@student.univaq.it>
2025-12-11 17:05:24 +0100

    aggiunta metodi Dungeon

--- src/main/java/GameWorld/dungeon.java
+++ src/main/java/GameWorld/dungeon.java
@@ -19,18 +19,16 @@ public void generateDungeon(){
     int numberOfRooms=10;
     int numberOfRandomRooms=0;
     while(numberOfRandomRooms<numberOfRooms){
+        Position p;
         int randomRow=random.nextInt(WIDTH);
         int randomCol=random.nextInt(HEIGHT);
-
-    if(dungeonMap[randomRow][randomCol]==null){
-        Position pos=new Position (randomRow,randomCol);
-        dungeonMap[randomRow][randomCol]=new Room (pos);
+        p=new Position(x,y);
         numberOfRandomRooms++;
 
     }
+    throw new RoomaMaxExceededException("Ops attenzione!Sembra che il numero massimo di stanze sia stato raggiunto!!");
         }
-    }
-            
+    
 
 public Room getRoom(int row,int col){
     if(row<0|| row>=WIDTH||col<0||col>=HEIGHT){
