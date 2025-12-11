8de642d41348cd7d390b728dd79350a70029691b
MartinaScorrano <martina.scorrano@student.univaq.it>
2025-12-11 11:07:59 +0100

    modifiche Dungeon

--- src/main/java/GameWorld/dungeon.java
+++ src/main/java/GameWorld/dungeon.java
@@ -1,15 +1,19 @@
-package GameWorld;
+package gameworld;
+import java.util.Random;
 
 public class Dungeon {
     private int width,height;
     private Room[][]dungeon;
+    private Random random;
 
-public Dungeon (int width,int height){
-    this.width=width;
-    this.height=height;
-    dungeon=new Room [width][height];
+public Dungeon (){
+    this.width=100;
+    this.height=100;
+    dungeon=new Room[width][height];
+    this.random=new Random();
+    generateDungeon();
 }
 public void generateDungeon(){
-    
+
 }   
 }
